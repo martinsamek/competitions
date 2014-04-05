87a108c8c0b7761a1b50a90c4442e1329e89b943
Martin SAMEK <martinsamek51@gmail.com>
2014-04-05 18:12:34 +0200

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Competitions::Application.config.secret_key_base = '4780503558c6d5e57158d32d914ab4409d71a2d99477fca2268ca47fdf1329af05c6486e0014b338edcd225fe0be2ccc818cc7aad504f073e9a6e0adcf65a128'
